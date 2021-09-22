7a5d311ba4599d95271bb7d06d197a50cb190839
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-22 17:17:28 +0200

    Ejercicio11

---
+++ ET01/src/es/florida/ET01/Ejercicio11.java
@@ -0,0 +1,24 @@
+//Ejercicio 11. Implementa un programa que dado un DNI (sin letra) por teclado devuelva el DNI con la letra correcta.
+
+package es.florida.ET01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) throws IOException {
+		
+		char[] arrayletras = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Introduce el numero de DNI sin la letra: ");
+		int numeroDNI = teclado.nextInt();
+		int resto = numeroDNI % 23;
+		System.out.println("La letra de tu DNI es: " + arrayletras[resto]);
+		teclado.close();
+		
+	}
+}
+
