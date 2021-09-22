027d82c2d169280048537d54dc79e12ee97dc5ec
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-22 12:42:47 +0200

    Creacion de un plantilla para los ejercicios

---
+++ ET01/src/es/florida/ET01/Plantilla.java
@@ -0,0 +1,17 @@
+//Ejercicio 
+
+package es.florida.ET01;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Plantilla {
+
+	public static void main(String[] args) throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		isr.close();
+		br.close();
+	}
+}
