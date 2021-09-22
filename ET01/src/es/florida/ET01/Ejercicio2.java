44b0d1a9774e65272887b80e16513a99d29be613
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-22 11:11:59 +0200

    Ejercicio2

---
+++ ET01/src/es/florida/ET01/Ejercicio2.java
@@ -0,0 +1,23 @@
+//Ejercicio2. Programa Java que lea un nombre desde teclado y muestre por pantalla un mensaje tipo “Hola xxxxx”.
+
+package es.florida.ET01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) throws IOException {
+		
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		System.out.print("Dime tu nombre: ");
+		String nombre = br.readLine();
+		System.out.println("Hola " + nombre);
+		isr.close();
+		br.close();
+		
+	}
+
+}
