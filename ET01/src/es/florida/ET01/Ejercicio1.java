6e9d89630891f9dc3d84542ff8d55afd65a06b07
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-22 11:01:17 +0200

    Ejercicio1

---
+++ ET01/src/es/florida/ET01/Ejercicio1.java
@@ -0,0 +1,13 @@
+// //Ejercicio1. Realiza un programa que a partir de dos números (introducidos en el código) muestre su suma por pantalla.
+
+package es.florida.ET01;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x= 2;
+		int y= 4;
+		System.out.println("La suma de x + y es: " + (x+y));
+	}
+}
