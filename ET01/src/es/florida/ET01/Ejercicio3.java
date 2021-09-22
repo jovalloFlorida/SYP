7e4fbd27b38386b3e9a8dca079dddd5b55033505
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-22 11:19:50 +0200

    Ejercicio3

---
+++ ET01/src/es/florida/ET01/Ejercicio3.java
@@ -0,0 +1,24 @@
+// Ejercicio3. Realiza un programa que lea dos números desde teclado y muestre la suma por pantalla
+
+package es.florida.ET01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		System.out.print("Introduce el primer numero para hacer la suma: ");
+		String numero1=br.readLine();
+		System.out.print("Introduce el segundo numero para hacer la suma: ");
+		String numero2=br.readLine();
+		System.out.print("La suma de los 2 numeros introducidos es: " + (Integer.parseInt(numero1) + Integer.parseInt(numero2)));
+		isr.close();
+		br.close();
+
+	}
+
+}
