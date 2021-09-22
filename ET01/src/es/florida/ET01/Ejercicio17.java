08c54a5810383c65325366d48c744d9097561d82
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-22 19:40:15 +0200

    //TODO comprobar el numero y la mayuscula

---
+++ ET01/src/es/florida/ET01/Ejercicio17.java
@@ -0,0 +1,27 @@
+//Ejercicio 17. Realiza un programa que controle los requisitos de una contraseña introducida por teclado (mínimo 5 caracteres, 1 número y 1 letra mayúscula).
+
+package es.florida.ET01;
+
+import java.util.Scanner;
+
+public class Ejercicio17 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Introduce una contraseña de minimo 5 caracteres: ");
+		String passwd = teclado.nextLine();
+		while (passwd.length() < 5) {
+			System.out.print("Introduce un minimo de 5 caracteres: ");
+			passwd = teclado.nextLine();
+		}
+		
+		//TODO comprobar el numero y la mayuscula
+		
+		System.out.println("Tu contraseña es: " + passwd);
+		
+		teclado.close();
+
+	}
+
+}
