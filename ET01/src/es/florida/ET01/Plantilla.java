920b994bd5453f56157c841251d26e65784f7b05
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-22 17:18:11 +0200

    Actualizacion de plantilla

--- ET01/src/es/florida/ET01/Plantilla.java
+++ ET01/src/es/florida/ET01/Plantilla.java
@@ -3,8 +3,10 @@
 package es.florida.ET01;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Plantilla {
 
