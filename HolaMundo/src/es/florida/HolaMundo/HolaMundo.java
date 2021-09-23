beae654f5f804363559fd42e13b4ee4bd7fd7f78
Jose Valverde <jovall@floridauniversitaria.es>
2021-09-23 12:51:35 +0200

    Mejora de codigo

--- HolaMundo/src/es/florida/HolaMundo/HolaMundo.java
+++ HolaMundo/src/es/florida/HolaMundo/HolaMundo.java
@@ -6,14 +6,13 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		
+		//System.out.println("Hola Mundo");
+		
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("Dime tu nombre: ");
 		String nombre = teclado.nextLine();
 		System.out.println("Hola " + nombre);
-		
-				
-		//System.out.println("Hola Mundo");
-
+		teclado.close();
 	}
 
 }
